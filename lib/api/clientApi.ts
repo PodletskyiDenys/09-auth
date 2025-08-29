ff355453dc4c570695a1665e72f4a70f3c4578df
PodletskyiDenys <denus.podletskiy@gmail.com>
2025-08-29 16:21:44 +0300

    new push

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,40 +1,50 @@
-import { UserMe, UserRequest } from '@/types/user';
-import { CheckSession } from '@/types/response';
-import { nextServer } from './api';
-import type { FetchNotesResponse, NewNoteData, Note, Params } from '../../types/note';
+import axios from "axios";
+import { UserMe, UserRequest } from "@/types/user";
+import { CheckSession } from "@/types/response";
+import type {
+  FetchNotesResponse,
+  NewNoteData,
+  Note,
+  Params,
+} from "@/types/note";
+
+export const nextServer = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000", // зміни на свій бекенд
+  withCredentials: true,
+});
 
 export const registration = async (user: UserRequest): Promise<UserMe> => {
-  const {data}= await nextServer.post<UserMe>(`/auth/register`, user);
+  const { data } = await nextServer.post<UserMe>("/auth/register", user);
   return data;
 };
 
 export const login = async (user: UserRequest): Promise<UserMe> => {
-  const {data}= await nextServer.post<UserMe>(`/auth/login`, user);
+  const { data } = await nextServer.post<UserMe>("/auth/login", user);
   return data;
 };
 
 export const logout = async () => {
-  const {data}= await nextServer.post(`/auth/logout`);
-  return data.success;   
+  const { data } = await nextServer.post<{ success: boolean }>("/auth/logout");
+  return data.success;
 };
 
 export async function checkSessionClient() {
-  const {data}= await nextServer.get<CheckSession>('/auth/session');
+  const { data } = await nextServer.get<CheckSession>("/auth/session");
   return data.success;
 }
 
 export async function userInfoClient() {
-  const {data} = await nextServer.get<UserMe>('/users/me');
+  const { data } = await nextServer.get<UserMe>("/users/me");
   return data;
 }
 
 export async function updateUser(user: Partial<UserMe>) {
-  const {data} = await nextServer.patch<UserMe>('/users/me', user);
+  const { data } = await nextServer.patch<UserMe>("/users/me", user);
   return data;
 }
 
 export async function fetchNotes(
-  searchValue: string = '',
+  searchValue: string = "",
   page: number = 1,
   tag?: string,
   perPage: number = 12
@@ -49,24 +59,23 @@ export async function fetchNotes(
     params.search = searchValue;
   }
 
-  const response = await nextServer.get<FetchNotesResponse>(`/notes`, {
-    params,    
+  const { data } = await nextServer.get<FetchNotesResponse>("/notes", {
+    params,
   });
-  return response.data;
+  return data;
 }
 
 export const addNote = async (noteData: NewNoteData): Promise<Note> => {
-  const response = await nextServer.post<Note>(`/notes`, noteData);
-  return response.data;
+  const { data } = await nextServer.post<Note>("/notes", noteData);
+  return data;
 };
 
 export const deleteNote = async (id: string): Promise<Note> => {
-  const response = await nextServer.delete<Note>(`/notes/${id}`);
-  return response.data;
+  const { data } = await nextServer.delete<Note>(`/notes/${id}`);
+  return data;
 };
 
 export async function fetchGetNoteById(id: string): Promise<Note> {
-  const response = await nextServer.get<Note>(`/notes/${id}`);
-  return response.data;
+  const { data } = await nextServer.get<Note>(`/notes/${id}`);
+  return data;
 }
-
